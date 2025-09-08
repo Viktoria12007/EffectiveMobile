47ee376f4dc6f1a72ff4fdba5d330495c7e6abad
Виктория Голубова <v.golubova@denexy.com>
2025-09-08 23:28:40 +0600

    Created entity 'User'.

---
+++ src/entities/user.ts
@@ -0,0 +1,42 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { IsBoolean, IsDate, IsEmail, IsIn, IsString, Length } from "class-validator";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsString()
+  @Length(2, 200)
+  @Column({
+    length: 200
+  })
+  fullName: string;
+
+  @IsDate()
+  @Column()
+  dateOfBirth: Date;
+
+  @IsEmail()
+  @Column({
+    unique: true,
+    select: false
+  })
+  email: string;
+
+  @IsString()
+  @Column({
+    select: false
+  })
+  password: string;
+
+  @IsIn(['admin', 'user'])
+  @Column()
+  role: 'admin' | 'user';
+
+  @IsBoolean()
+  @Column({
+    default: false
+  })
+  isActive: boolean;
+}
