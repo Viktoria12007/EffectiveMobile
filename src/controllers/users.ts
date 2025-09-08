a8f9b0b50337a41d4e351e83424a9a5bbf5626a0
Виктория Голубова <v.golubova@denexy.com>
2025-09-08 23:30:41 +0600

    Created controller users.ts with methods: 'createUser', 'login' and 'getUsers'.

---
+++ src/controllers/users.ts
@@ -0,0 +1,74 @@
+import { User } from "../entities/user";
+import { UpdateUserDto } from "../entities/dto/update-user.dto";
+import { Request, Response, NextFunction } from 'express';
+import bcrypt from 'bcryptjs';
+import ConflictError from "../errors/conflict-error";
+import BadRequestError from "../errors/bad-request-error";
+import jwt from 'jsonwebtoken';
+import UnauthorizedError from "../errors/unauthorized-error";
+import { AppDataSource } from "../data-source";
+
+const userRepository = AppDataSource.getRepository(User);
+
+export function createUser(req: Request, res: Response, next: NextFunction) {
+  const { fullName, dateOfBirth, email, password, role } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => userRepository.create({ fullName, dateOfBirth, email, role, password: hash }))
+    .then((data) => res.status(201).send(data))
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        next(new BadRequestError(error.message));
+      } else if (error.code === 11000) {
+        next(new ConflictError('Пользователь с данным email уже существует'));
+      } else {
+        next(error);
+      }
+    })
+}
+
+export async function login(req: Request, res: Response, next: NextFunction) {
+  try {
+    const { email, password } = req.body;
+    const user = await userRepository.findOne({ where: { email }, select: { password: true } });
+    if (!user) {
+     throw new UnauthorizedError('Неправильные почта или пароль');
+    }
+    const matched = await bcrypt.compare(password, user.password);
+    if (!matched) {
+      throw new UnauthorizedError('Неправильные почта или пароль')
+    }
+    const token = jwt.sign({ _id: user.id }, process.env.JWT_SECRET);
+    return res.cookie('jwt', token, {
+      maxAge: 3600000,
+      httpOnly: true,
+      sameSite: true,
+    }).send({ token })
+  } catch (e) {
+    next(e);
+  }
+}
+
+export async function getUserById(req: Request, res: Response, next: NextFunction) {
+  // const user = await userRepository.findOneBy({ id });
+  // if (!user) {
+  //   throw new NotFoundError("Такого пользователя не существует");
+  // }
+  // return res.send(user);
+}
+
+export async function getUsers(req: Request, res: Response, next: NextFunction) {
+  try {
+    const users = await userRepository.find();
+    res.send(users);
+  } catch(e) {
+    next(e);
+  }
+}
+
+export function blockUser(id: number, updateUserDto: UpdateUserDto) {
+  return userRepository.update(id, updateUserDto);
+}
+
+export function isAdmin(user: User) {
+  return user.role === "admin";
+}
