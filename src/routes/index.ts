bf4bb660ee18c58acb1c16d4708e4dbba0face35
Виктория Голубова <v.golubova@denexy.com>
2025-09-10 11:05:50 +0600

    Added void middlewares 'auth', 'checkIsActive'. Added checking for non-existent routes.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,22 @@
 import { Router } from "express";
 import { createUser, login } from "../controllers/users";
 import userRoute from "./users";
+import auth from "../middlewares/auth";
+import { NextFunction, Request, Response } from "express";
+import NotFoundError from "../errors/not-found-error";
+import checkIsActive from "../middlewares/checkIsActive";
 
 const router = Router();
 
 router.post("/signup", createUser);
 router.post("/signin", login);
 
+router.use(auth);
+router.use(checkIsActive);
 router.use("/users", userRoute);
+
+router.use((req: Request, res: Response, next: NextFunction) => {
+  next(new NotFoundError('Маршрут не найден'));
+});
 
 export default router;
