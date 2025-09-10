3de4ce593e980e70c2f571cb25e7cceb034f397b
Виктория Голубова <v.golubova@denexy.com>
2025-09-10 11:01:59 +0600

    Added void middleware 'cookieParser' and import 'dotenv/config'.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
+import 'dotenv/config';
 import express from 'express';
 import "reflect-metadata";
 import routes from "./routes";
 import { AppDataSource } from "./data-source";
 import cors from 'cors';
+import cookieParser from "cookie-parser";
 
-const PORT = parseInt(process.env.PORT as string, 10) || 3000;
 const app = express();
 AppDataSource.initialize();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`EffectiveMobile app listening on port ${PORT}`)
+app.listen(process.env.PORT, () => {
+  console.log(`EffectiveMobile app listening on port ${process.env.PORT}`)
 })
 
