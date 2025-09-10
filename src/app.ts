b24b41050fc90fde740e1292160d7d3489c7a42a
Виктория Голубова <v.golubova@denexy.com>
2025-09-10 13:01:17 +0600

    Renamed from index.ts. Added void middleware 'errorHandler'.

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import routes from "./routes";
 import { AppDataSource } from "./data-source";
 import cors from 'cors';
 import cookieParser from "cookie-parser";
+import errorHandler from "./middlewares/error-handler";
 
 const app = express();
 AppDataSource.initialize();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(routes);
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
   console.log(`EffectiveMobile app listening on port ${process.env.PORT}`)
