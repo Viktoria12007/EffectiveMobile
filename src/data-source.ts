d7c06d365ab344ba8d9270b9dc90ee3b7e6d53cd
Виктория Голубова <v.golubova@denexy.com>
2025-09-10 10:30:05 +0600

    Removed alternatives.

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,11 +3,11 @@ import { User } from "./entities/user";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.POSTGRES_HOST || "localhost",
-  port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
-  username: process.env.POSTGRES_USERNAME || "effective_mobile",
-  password: process.env.POSTGRES_PASSWORD || "effective_mobile",
-  database: process.env.POSTGRES_DATABASE || "effective_mobile",
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT, 10),
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
   entities: [User],
   synchronize: true
 });
